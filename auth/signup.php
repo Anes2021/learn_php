d67753f4c99c6f99247ed4ac3af3f1af80f5b508
Anes2021 <hellalet.anes@gmail.com>
2024-09-30 21:44:17 -0300

    fix problems

--- auth/signup.php
+++ auth/signup.php
@@ -4,16 +4,36 @@ include "../connect.php";
 
 //we use filterRequest Function for SECURITY "i made it in function.php file using htmlspecialchars, and we also use $_POST methode becuase it is more secure"
 
-    $username = filterRequest("user_name");
-    $email = filterRequest("user_email");
-    $password = filterRequest("user_pass");
+$username = filterRequest("user_name");
+$email = filterRequest("user_email");
+$password = filterRequest("user_pass");
 
+try {
     $stmt = $con->prepare("INSERT INTO `users`(`user_name`, `user_pass`, `user_email`) VALUES (?,?,?)");
-    $stmt->execute(array($username, $password, $email));    
-    $count =$stmt->rowCount();
+    $stmt->execute(array($username, $password, $email));
+    $count = $stmt->rowCount();
 
-    if($count > 0) {
-        echo "user added!";
-    }else {
-        echo "something went wrong!";
+    if ($count > 0) {
+        // Return a JSON response indicating success
+        echo json_encode([
+            "status" => "successful",
+            "username" => $username,
+            "email" => $email,
+            "password" => $password
+        ]);
+    } else {
+        // Return a JSON response indicating failure
+        echo json_encode([
+            "status" => "failed",
+            "username" => "Something went wrong!",
+            "email" => "Something went wrong!",
+            "password" => "Something went wrong!"
+        ]);
     }
+} catch (PDOException $e) {
+    // Catch exceptions and return an error message
+    echo json_encode([
+        "status" => "failed",
+        "message" => "An error occurred: " . $e->getMessage()
+    ]);
+}
